44b724904c5ca02dc2049f558c4cfefefc5efa58
dwikbtn <dwikbtn15@gmail.com>
2021-09-02 06:16:03 +0800

    adding parallax

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import { Parallax } from "react-parallax";
 import style from "./app.module.scss";
 import Explore from "./components/Explore/Explore";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import bg from "./img/landing.jpg";
 import "./style/main.scss";
 
 function App() {
   return (
     <>
       <Navbar />
-      <section className={style["hero-background"]}>
-        <div className="container">
-          <Hero />
-        </div>
-      </section>
+      <Parallax Parallax bgImage={bg} strength={400} blur={{ min: -1, max: 2 }}>
+        <section className={style["hero-background"]}>
+          <div className="container">
+            <Hero />
+          </div>
+        </section>
+      </Parallax>
       <section className="explore">
         <Explore />
       </section>
